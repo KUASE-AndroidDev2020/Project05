547ac005a6f30217f718aa2f1228e12004f31c10
Yuto01 <2020m011@kuas.ac.jp>
2020-07-11 15:51:58 +0900

    Add a program about buttons

--- app/src/main/java/com/example/project05/MainActivity.java
+++ app/src/main/java/com/example/project05/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.project05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToUranaiSelect(View view) {
+        Intent toUranai=new Intent(this, UranaiSelect.class);
+        startActivity(toUranai);
     }
 }
