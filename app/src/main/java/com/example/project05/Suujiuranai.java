558736cb2a3987d1ef0413a777d5fa64bf2e0e29
Rui0405 <2020m070@kuas.ac.jp>
2020-07-13 03:05:07 +0900

    create suujiuranai and nextbutton

---
+++ app/src/main/java/com/example/project05/Suujiuranai.java
@@ -0,0 +1,14 @@
+package com.example.project05;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Suujiuranai extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suujiuranai);
+    }
+}
