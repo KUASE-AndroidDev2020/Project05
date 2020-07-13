fa611ab23533b7eb1363da424375e7276f0a932a
Yuto01 <2020m011@kuas.ac.jp>
2020-07-13 12:04:32 +0900

    Add a program about buttons 2

--- app/src/main/java/com/example/project05/torannpuuranaikekka.java
+++ app/src/main/java/com/example/project05/torannpuuranaikekka.java
@@ -2,7 +2,9 @@ package com.example.project05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class torannpuuranaikekka extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class torannpuuranaikekka extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_torannpuuranaikekka);
+    }
+
+    public void goToMainActivity(View view) {
+        Intent toUranai=new Intent(this, MainActivity.class);
+        startActivity(toUranai);
     }
 }
