ca2b0edcd7cebc4379b415ecf36a588c0eef98ec
Rui0405 <2020m070@kuas.ac.jp>
2020-07-14 23:51:56 +0900

    create sinnritest

---
+++ app/src/main/java/com/example/project05/sinnrikekka2.java
@@ -0,0 +1,14 @@
+package com.example.project05;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sinnrikekka2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sinnrikekka2);
+    }
+}
